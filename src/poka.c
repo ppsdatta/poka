705ecd8bc7ce43e3c83c13e396443ad8732cc386
Sourav Datta <sdatta@planview.com>
2021-08-27 17:01:08 +0530

    Add some util functions

--- src/poka.c
+++ src/poka.c
@@ -4,12 +4,13 @@
 #include <ctype.h>
 #include <math.h>
 
-
 #define MAX 100
-enum {
+
+enum
+{
     END,
-    ERROR, 
-    NUMBER, 
+    ERROR,
+    NUMBER,
     PLUS,
     MINUS,
     MULT,
@@ -34,7 +35,6 @@ enum {
     CCURLY
 };
 
-
 static char token[MAX];
 
 int gettoken(void)
@@ -45,22 +45,29 @@ int gettoken(void)
     memset(token, 0, MAX);
 
     ch = getchar();
-    while (isspace(ch)) {
+    while (isspace(ch))
+    {
         ch = getchar();
     }
 
-    if (ch == EOF) return END;
+    if (ch == EOF)
+        return END;
 
-    if ((ch >= '0' && ch <= '9') || (ch == '.')) {
-        while ((ch >= '0' && ch <= '9') || (ch == '.')) {
-            if ((ch == '.') && (decimal != 0)) {
+    if ((ch >= '0' && ch <= '9') || (ch == '.'))
+    {
+        while ((ch >= '0' && ch <= '9') || (ch == '.'))
+        {
+            if ((ch == '.') && (decimal != 0))
+            {
                 return ERROR;
             }
-            else if (decimal != 0) {
+            else if (decimal != 0)
+            {
                 decimal = 1;
             }
 
-            if (index == MAX - 2) break;
+            if (index == MAX - 2)
+                break;
             token[index++] = ch;
             ch = getchar();
         }
@@ -69,9 +76,12 @@ int gettoken(void)
         return NUMBER;
     }
 
-    if (isalpha(ch)) {
-        while (isalpha(ch) || isdigit(ch) || ch =='_') {
-            if (index == MAX - 2) break;
+    if (isalpha(ch))
+    {
+        while (isalpha(ch) || isdigit(ch) || ch == '_')
+        {
+            if (index == MAX - 2)
+                break;
             token[index++] = ch;
             ch = getchar();
         }
@@ -82,99 +92,129 @@ int gettoken(void)
 
     type = ERROR;
 
-    switch (ch) {
-        case '+':
-            type = PLUS;
-            break;
-        case '-':
-            type = MINUS;
-            break;
-        case '*':
-            type = MULT;
-            break;
-        case '/':
-            type = DIV;
-            break;
-        case '%':
-            type = REM;
-            break;
-        case '^':
-            type = POW;
-            break;
-        case ':':
-            ch = getchar();
-            if (ch != EOF && ch == '=') {
-                type = ASSIGN;
-            }
-            else {
-                ungetc(ch, stdin);
-                type = COLON; 
-            }
-            break;
-        case ';':
-            type = SEMI;
-            break;
-        case '=':
-            type = EQ;
-            break;
-        case '<':
-            ch = getchar();
-            if (ch == '>') {
-                type = NOT;
-            }
-            else if (ch == '=') {
-                type = LE;
-            }
-            else {
-                type = LT;
-                ungetc(ch, stdin);
-            }
-            break;
-        case '>':
-            ch = getchar();
-            if (ch == '=') {
-                type = GE;
-            }
-            else {
-                type = GT;
-                ungetc(ch, stdin);
-            }
-            break;
-        case '(':
-            type = OPAREN;
-            break;
-        case ')':
-            type = CPAREN;
-            break;
-        case '[':
-            type = OBRACKET;
-            break;
-        case ']':
-            type = CBRACKET;
-            break;
-        case '{':
-            type = OCURLY;
-            break;
-        case '}':
-            type = CCURLY;
-            break;
-        default:
-            break;
+    switch (ch)
+    {
+    case '+':
+        type = PLUS;
+        break;
+    case '-':
+        type = MINUS;
+        break;
+    case '*':
+        type = MULT;
+        break;
+    case '/':
+        type = DIV;
+        break;
+    case '%':
+        type = REM;
+        break;
+    case '^':
+        type = POW;
+        break;
+    case ':':
+        ch = getchar();
+        if (ch != EOF && ch == '=')
+        {
+            type = ASSIGN;
+        }
+        else
+        {
+            ungetc(ch, stdin);
+            type = COLON;
+        }
+        break;
+    case ';':
+        type = SEMI;
+        break;
+    case '=':
+        type = EQ;
+        break;
+    case '<':
+        ch = getchar();
+        if (ch == '>')
+        {
+            type = NOT;
+        }
+        else if (ch == '=')
+        {
+            type = LE;
+        }
+        else
+        {
+            type = LT;
+            ungetc(ch, stdin);
+        }
+        break;
+    case '>':
+        ch = getchar();
+        if (ch == '=')
+        {
+            type = GE;
+        }
+        else
+        {
+            type = GT;
+            ungetc(ch, stdin);
+        }
+        break;
+    case '(':
+        type = OPAREN;
+        break;
+    case ')':
+        type = CPAREN;
+        break;
+    case '[':
+        type = OBRACKET;
+        break;
+    case ']':
+        type = CBRACKET;
+        break;
+    case '{':
+        type = OCURLY;
+        break;
+    case '}':
+        type = CCURLY;
+        break;
+    default:
+        break;
     }
 
     return type;
 }
 
+static int lasttoken = -1;
+
+int nexttoken(void)
+{
+    int token;
+
+    if (lasttoken == -1)
+    {
+        return gettoken();
+    }
+    else
+    {
+        token = lasttoken;
+        lasttoken = -1;
+        return token;
+    }
+}
+
+void savetoken(int tok)
+{
+    lasttoken = tok;
+}
 
 int main()
 {
     int type = gettoken();
 
-    while (type != ERROR && type != END) {
+    while (type != ERROR && type != END)
+    {
         printf("%d %s\n", type, token);
         type = gettoken();
     }
 
     return 0;
 }
-
